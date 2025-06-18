213272baaf0dc19d9f52c25a2c6fae528a7854de
SharlottaL <botanichkagigantika@gmail.com>
2025-06-18 16:31:56 +0300

    CALC day1

--- WinAPI/MainWindow/maincpp.cpp
+++ WinAPI/MainWindow/maincpp.cpp
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include"resource.h"
 
+#define IDC_STATIC   1000
+#define IDC_EDIT     1001
+#define IDC_BUTTON   1002
+
+
 const char g_sz_CLASS_NAME[] = "Параметры Окна:";
 
 INT WINAPI WndProc(HWND hwnd, UINT umsg, WPARAM wParam, LPARAM lParam);
@@ -19,14 +24,15 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	//wClass.hIcon = (HICON)LoadImage(hInstance, "zay.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	//wClass.hIconSm = (HICON)LoadImage(hInstance, "ezik.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wClass.hCursor = (HCURSOR)LoadImage(
-		hInstance, 
+		hInstance,
 		"rabbit.cur",
 		IMAGE_CURSOR,
 		LR_DEFAULTSIZE, LR_DEFAULTSIZE,
 		LR_LOADFROMFILE
 	);
-	HBRUSH hBrush = CreateSolidBrush(RGB(180, 90, 130));
-	wClass.hbrBackground = hBrush;
+	//HBRUSH hBrush = CreateSolidBrush(RGB(180, 90, 130));
+	//wClass.hbrBackground = hBrush;
+	wClass.hbrBackground = (HBRUSH)22;
 
 	wClass.hInstance = hInstance;
 	wClass.lpfnWndProc = (WNDPROC)WndProc;
@@ -38,10 +44,10 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		MessageBox(NULL, "Class registration failed", "", MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	
+
 	/*RECT workArea;
 	SystemParametersInfoA(SPI_GETWORKAREA, 0, &workArea, 0);
-	
+
 	int weith = (workArea.right - workArea.left) * 0.75;
 	int hegth = (workArea.bottom - workArea.top) * 0.75;
 	int x = (workArea.right + workArea.left - weith) / 2;
@@ -50,7 +56,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	sprintf_s(size, sizeof(size), "%s [Размер: %dx%d, Позиция: (%d,%d)]", g_sz_CLASS_NAME, weith, hegth, x, y);*/
 	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
 	INT screen_heigth = GetSystemMetrics(SM_CXSCREEN);
-	
+
 	INT window_width = screen_width * .75;
 	INT window_hegth = screen_heigth * 3 / 4;
 	INT window_start_x = screen_width / 8;
@@ -84,8 +90,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-	return msg.message;
-	DeleteObject(hBrush);
+	return msg.wParam;
+	//DeleteObject(hBrush);
 }
 
 INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -93,7 +99,49 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
-		break;
+	{
+	HWND hStatic = CreateWindowEx
+		(
+			NULL,
+			"Static",
+			"Этот staticText  создан при помощи функции CreateWindowEx().",
+			WS_CHILD | WS_VISIBLE,
+			10, 10,
+			550, 25,
+			hwnd,
+			(HMENU)IDC_STATIC,
+			GetModuleHandle(NULL),
+			NULL
+			);
+	CreateWindowEx(
+		NULL, "EDIT", "",
+		WS_CHILD | WS_VISIBLE|WS_BORDER|ES_CENTER, 
+		10, 50,
+		550, 22,
+		hwnd,
+		(HMENU)IDC_EDIT,
+		GetModuleHandle(NULL),
+		NULL
+
+	);
+	CreateWindowEx
+	(
+		NULL, "Button", "Применить",
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		450, 75,
+		110, 25,
+		hwnd,
+		(HMENU)IDC_BUTTON,
+		GetModuleHandle(NULL),
+		NULL
+	);
+	}
+	break;
+	/*case WM_PAINT:
+	{
+		SendMessage(GetDlgItem(hwnd, 1000), WM_SETTEXT, 0, (LPARAM)"Этот staticText  создан при помощи функции CreateWindowEx().");
+	}
+	break;*/
 	case WM_MOVE:
 	case WM_SIZE:
 	{
@@ -106,13 +154,27 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		sprintf
 		(
 			sz_title,
-			"%s - Position: %ix%i, Size:%iX%i", 
+			"%s - Position: %ix%i, Size:%iX%i",
 			g_sz_CLASS_NAME,
-			window_rect.left,window_rect.top,
+			window_rect.left, window_rect.top,
 			window_width, window_height);
 		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
 	}
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_STATIC:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			HWND hStatic = GetDlgItem(hwnd, IDC_STATIC);
+			CONST INT SIZE = 1024;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hStatic, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+			break;
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
